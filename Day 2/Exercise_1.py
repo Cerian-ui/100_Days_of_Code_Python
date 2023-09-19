5ca24d03730998ca8c0de7e253bc0e925191e740
Bárbara Augusta <73501565+Cerian-ui@users.noreply.github.com>
2023-09-19 00:09:44 -0300

    Create Exercise_1.py

---
+++ Day 2/Exercise_1.py
@@ -0,0 +1,15 @@
+#Write a program that adds the digits in a 2 digit number.
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+
+first_num = two_digit_number[0]
+second_num = two_digit_number[1]
+
+new_first_num = int(first_num)
+new_second_num = int(second_num)
+
+print(new_first_num + new_second_num)
