dc2d6fe486a42ee9d493a76859a5ad6bcc7228d1
Bárbara Augusta <73501565+Cerian-ui@users.noreply.github.com>
2023-09-21 22:17:08 -0300

    Update Exercise_4.py

--- Day 5/Exercise_4.py
+++ Day 5/Exercise_4.py
@@ -9,7 +9,7 @@
 # And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
 
 #Write your code below this row 👇
-words = 0
+
 for number in range (1,101):
     if (number % 3 == 0) and (number % 5 == 0):
         number = "FizzBuzz" 
