7cf2aef1996864efed36a1e4981803b6c2176889
Bárbara Augusta <73501565+Cerian-ui@users.noreply.github.com>
2023-09-21 22:05:55 -0300

    Create Exercise_3.py

---
+++ Day 5/Exercise_3.py
@@ -0,0 +1,7 @@
+#You are going to write a program that calculates the sum of all the even numbers from 1 to 100.
+
+#Write your code below this row 👇
+even = 0
+for number in range(2,101,2):
+    even += number
+print(even)
