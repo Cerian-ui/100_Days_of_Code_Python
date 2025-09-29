1db28b60abe22f9af9e04b0af72ab12c2b839ff6
Bárbara Augusta <73501565+Cerian-ui@users.noreply.github.com>
2025-09-29 08:46:07 -0300

    Update Exercise_1.py

--- Day 1/Exercise_1.py
+++ Day 1/Exercise_1.py
@@ -3,8 +3,11 @@ print("The function is declared like this:")
 print("print('what to print')")
 
 #Second exercise
-print ("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
-print ("2. Knead the dough for 10 minutes.")
-print ("3. Add 3g of Salt.")
-print ("4. Leave to rise for 2 hours.")
-print ("5. Bake at 200 degrees C for 30 minutes.")
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+#It's possible to do the same thing but instead of using multiple print command, we can use \n after each phrase.
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.")
